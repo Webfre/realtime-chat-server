4fede516c0551b437263e194943909d34b9c5f25
Roman <109579287+Webfre@users.noreply.github.com>
2023-04-07 15:50:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ io.on('connection', (socket) => {
   socket.on('join', ({ name, room }) => {
     socket.join(room);
 
-    console.log(socket);
     // Проверка на регистрацию 
     const { user } = addUser({ name, room });
 
